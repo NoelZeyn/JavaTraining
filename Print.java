e318073b25589aaa0ea1fe7f9a8b716fa6d58572
NoelZeyn <127940099+NoelZeyn@users.noreply.github.com>
2026-01-18 21:10:21 +0700

    Time to enter Java Methods

--- Print.java
+++ Print.java
@@ -2,20 +2,32 @@ import java.util.Scanner;
 
 public class Print {
 
-    final int[] age = {10, 20, 30, 40, 50};
-
+    
     public static void main(String[] args) {
+        int[] age = { 10, 20, 30, 40, 50 };
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan nama Anda: ");
         String nama = input.next();
         System.out.println("Hello " + nama);
-        Print p = new Print();
+        // Print p = new Print();
+        int ab = 15;
+        // System.out.println(p.getAgeLength());
 
-        System.out.println(p.age.length);
-
-        for (int i = 0; i < p.age.length; i++) {
-            System.out.println(p.age[i]);
+        for (int i = 0; i < age.length; i++) {
+            // System.out.println(age[i]);
+            ab += age[i];
+            System.out.println("Jumlah sementara: " + ab);
+            if (ab > 150) {
+                System.out.println("Jumlah melebihi 150, harap diperhatikan");
+                break;
+            } else {
+                System.out.println("Jumlah tidak melebihi 150, lanjutkan");
+                continue;
+            }
+            
         }
         input.close();
     }
 }
+
+
